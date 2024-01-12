82ae69a6fb4e7af500997d0fb1c9be0d05e441d0
Rajashekhar Nandeppa Tuppad <rajashekharthuppad@gmail.com>
2024-01-12 15:49:45 +0530

    project

---
+++ src/main/java/com/example/demo/services/UserServiceImplementation.java
@@ -0,0 +1,58 @@
+package com.example.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.entities.Users;
+import com.example.demo.repositories.UsersRepository;
+
+@Service
+public class UserServiceImplementation implements UserService {
+
+   @Autowired
+   UsersRepository repo;
+   @Override
+    public String addUser(Users user) {
+        repo.save(user);
+        return "User added successfully";
+    }
+   
+   @Override
+   public boolean emailExists(String email) {
+	if(repo.findByEmail(email)==null) {
+	return false;
+	}
+	else {
+		return true;
+}
+   
+}
+   
+@Override
+public boolean validateUser(String email, String password) {
+	// TODO Auto-generated method stub
+	Users user=repo.findByEmail(email);
+	String db_pass=user.getPassword();
+	if(password.equals(db_pass)) {
+	return true;
+}else {
+	return false;
+}  
+}
+@Override
+public String getRole(String email) {
+	// TODO Auto-generated method stub
+	Users user=repo.findByEmail(email);
+	return user.getRole();
+}
+@Override
+public Users getUser(String email) {
+	// TODO Auto-generated method stub
+	return  repo.findByEmail(email);
+}
+@Override
+public void updateUser(Users user) {
+	// TODO Auto-generated method stub
+	repo.save(user);
+	
+}
+}
